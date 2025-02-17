8ce0b0d5c237544ed3da84aa928a9f276c1be8bc
Calum Gordon <calumrossgordon@hotmail.com>
2025-02-17 17:57:03 +0100

    Updated biases to reflect DESI DR1

--- lyaforecast/analytic_bias_McD2003.py
+++ lyaforecast/analytic_bias_McD2003.py
@@ -1,43 +1,47 @@
 import numpy as np
 
 class McDonald2003:
+    """Class to store analytic formulae for biases of Lya P3D, including non-linear corrections.
+        These will later be handled by ForestFlow, currently parameter values are out-of-date."""
 
-    def D_NL_hMpc(self,k_hMpc):
+    def _get_non_linear_corr(self,k_hMpc):
         """Non-linear correction"""
-        k_nl=6.40
-        alpha_nl=0.569 
-        return pow(k_hMpc/k_nl, alpha_nl)
+        k_nl = 6.40
+        alpha_nl = 0.569 
+        return pow(k_hMpc / k_nl, alpha_nl)
 
-    def D_p_hMpc(self,k_hMpc):
+    def _get_pressure_corr(self,k_hMpc):
         """Pressure correction"""
-        k_p=15.3
-        alpha_p=2.01
-        return pow(k_hMpc/k_p, alpha_p)
+        k_p = 15.3
+        alpha_p = 2.01
+        return pow(k_hMpc / k_p, alpha_p)
 
-    def D_v_hMpc(self,k_hMpc, mu):
+    def _get_non_linear_velo(self,k_hMpc, mu):
         """Non-linear velocities"""
-        k_v0=1.220
-        alpha_v=1.50
-        k_vv=0.923
-        alpha_vv=0.451
-        kpar = k_hMpc*mu
-        kv = k_v0 * pow(1+k_hMpc/k_vv, alpha_vv)
+        k_v0 = 1.220
+        alpha_v = 1.50
+        k_vv = 0.923
+        alpha_vv = 0.451
+        kpar = k_hMpc * mu
+        kv = k_v0 * pow(1 + k_hMpc / k_vv, alpha_vv)
         return pow(kpar/kv, alpha_v)
     
     def _get_density_bias(self,z):
-        """Linear density bias as a function of redshift"""
+        """Linear density bias as a function of redshift, 
+            values from DESI Collaboration et al., 2024b"""
         #alpha=1.25
         alpha = 2.9
-        bias_zref = -np.sqrt(0.0173)
-        zref = 2.25
-        return bias_zref * ((1+z)/(1+zref))**alpha
+        bias_zref = -0.1078
+        zref = 2.33
+        return bias_zref * ((1 + z)/(1 + zref))**alpha
 
     def _get_beta_rsd(self,z):
-        """Linear RSD anisotropy parameter as a function of redshift"""
+        """Linear RSD anisotropy parameter as a function of redshift,
+        values from DESI Collaboration et al., 2024b"""
         alpha = 0.0
-        beta_zref = 1.58
-        zref = 2.25
-        return beta_zref*((1+z)/(1+zref))**alpha
+        beta_zref = 1.743
+        zref = 2.33
+        return beta_zref*((1 + z)/(1 + zref))**alpha
 
     def _small_scale_correction(self,k_hmpc,mu):
         """Analytic formula for small-scales correction to Lyman alpha P3D(z,k,mu) 
@@ -46,7 +50,10 @@ class McDonald2003:
             Values are cosmology dependent, but we ignore it here.
             Wavenumbers in h/Mpc. """
         # this will go in the exponent
-        texp = self.D_NL_hMpc(k_hmpc) - self.D_p_hMpc(k_hmpc) - self.D_v_hMpc(k_hmpc,mu)
+        texp = (self._get_non_linear_corr(k_hmpc) 
+                - self._get_pressure_corr(k_hmpc)
+                - self._get_non_linear_velo(k_hmpc,mu))
+        
         return np.exp(texp)
 
     def compute_bias(self,z,k_hmpc,mu,linear=True):
@@ -59,7 +66,7 @@ class McDonald2003:
             Wavenumbers in h/Mpc. """
         b_delta = self._get_density_bias(z)
         beta = self._get_beta_rsd(z)
-        kaiser = pow(b_delta*(1+beta*pow(mu,2)),2)
+        kaiser = (b_delta * (1+ beta * mu**2))**2
         if linear:
             return kaiser
         else:
