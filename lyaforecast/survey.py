83b2a86d1564d45b2fb440bde574d025849516f6
Julien Guy <jguy@lbl.gov>
2026-08-31 15:02:56 -0700

    can specify redshift bin edges

--- lyaforecast/survey.py
+++ lyaforecast/survey.py
@@ -41,6 +41,13 @@ class Survey:
         if self.band not in self.BAND_OPTIONS:
             raise ValueError(f'Please choose from accepted bandpasses: {self.BAND_OPTIONS}')
 
+
+    def split_str(self,vals) :
+        tmp2=list()
+        for tmp in vals.split(" ") :
+            tmp2 += tmp.split(",")
+        return tmp2
+
     def _get_z_bins(self, config):
         """Parse redshift bin edges and centres from config.
 
@@ -53,15 +60,24 @@ class Survey:
         self.zmin = survey_cfg.getfloat('z bin min', 2)
         self.zmax = survey_cfg.getfloat('z bin max', 4)
         self.num_z_bins = survey_cfg.getint('num z bins', 1)
+        z_bin_edges = survey_cfg.get('z bin edges', None)
         z_bin_centres = survey_cfg.get('z bin centres', None)
-
-        if z_bin_centres is not None:
-            self.z_bin_centres = np.array(z_bin_centres.split(",")).astype(float)
+        if z_bin_edges is not None:
+            assert(z_bin_centres==None) # cannot define both
+            z_bin_edges = np.array(self.split_str(z_bin_edges)).astype(float)
+            z_bin_min=z_bin_edges[:-1]
+            z_bin_max=z_bin_edges[1:]
+            self.z_bin_centres = (z_bin_min+z_bin_max)/2.
+            self.z_bin_edges   = np.array([z_bin_min,z_bin_max])
+            self.num_z_bins    = self.z_bin_centres.size
+        elif z_bin_centres is not None:
+            self.z_bin_centres = np.array(self.split_str(z_bin_centers)).astype(float)
             dz = np.zeros(self.z_bin_centres.size)
             dz[1:-1] = (self.z_bin_centres[2:] - self.z_bin_centres[:-2]) / 2.
             dz[0] = self.z_bin_centres[1] - self.z_bin_centres[0]
             dz[-1] = self.z_bin_centres[-1] - self.z_bin_centres[-2]
             self.z_bin_edges = np.array([self.z_bin_centres - dz / 2, self.z_bin_centres + dz / 2])
+            self.num_z_bins    = self.z_bin_centres.size
         else:
             z_list = np.linspace(self.zmin, self.zmax, self.num_z_bins + 1)
             self.z_bin_edges = np.array(
